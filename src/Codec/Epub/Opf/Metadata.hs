aeda39f3e091df66ffcf9cf63fe84ac4e9ff8372
Dino Morelli <dino@ui3.info>
2010-05-06 14:57:32 +0000

    Initial crack at changing this over from TagSoup to HXT for metadata parsing

--- src/Codec/Epub/Opf/Metadata.hs
+++ src/Codec/Epub/Opf/Metadata.hs
@@ -3,36 +3,104 @@
 -- Author: Dino Morelli <dino@ui3.info>
 
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE Arrows, NoMonomorphismRestriction #-}
 
 module Codec.Epub.Opf.Metadata
    where
 
 import Control.Monad.Error
 import HSH.Command
-import Text.HTML.TagSoup
 import Text.Printf
+import Text.XML.HXT.Arrow
 
+
+data Title = Title
+   (Maybe String) -- xml:lang
+   String         -- content
+   deriving Show
 
 data Creator = Creator
-   String   -- role
-   String   -- creator
+   (Maybe String) -- opf:role
+   (Maybe String) -- opf:file-as
+   String         -- content
    deriving Show
 
-type Date =
-   ( String    -- event
-   , String    -- date value
-   )
+data Date = Date
+   (Maybe String) -- opf:event
+   String         -- content
+   deriving Show
+
+data Id = Id
+   String         -- id
+   (Maybe String) -- opf:scheme
+   String         -- content
+   deriving Show
 
 data EpubMeta = EpubMeta
-   { emTitle :: String
---   , emLanguage :: String
---   , emIdentifier :: String
-   , emCreator :: Creator
---   , emContributor :: String
---   , emDescription :: String
+   { emTitles :: [Title]   -- one required
+   , emCreators :: [Creator]
+   , emContributors :: [Creator]
+   , emSubjects :: [String]
+   , emDescription :: Maybe String
+   , emPublisher :: Maybe String
    , emDates :: [Date]
+   , emType :: Maybe String
+   , emFormat :: Maybe String
+   , emId :: [Id]          -- one required
+   , emSource :: Maybe String
+   , emLang :: [String]    -- one required
+   , emRelation :: Maybe String
+   , emCoverage :: Maybe String
+   , emRights :: Maybe String
    }
    deriving Show
+
+emptyEpubMeta = EpubMeta
+   { emTitles = []   -- one required
+   , emCreators = []
+   , emContributors = []
+   , emSubjects = []
+   , emDescription = Nothing
+   , emPublisher = Nothing
+   , emDates = []
+   , emType = Nothing
+   , emFormat = Nothing
+   , emId = []       -- one required
+   , emSource = Nothing
+   , emLang = []     -- one required
+   , emRelation = Nothing
+   , emCoverage = Nothing
+   , emRights = Nothing
+   }
+
+
+-- HXT helpers
+
+hxtParseParams = [(a_validate, v_0)]
+
+atTag tag = deep (isElem >>> hasName tag)
+
+atQTag tag = deep (isElem >>> hasQName tag)
+
+text = getChildren >>> getText
+
+notNullA = isA $ not . null
+
+mbGetAttrValue n =
+   (getAttrValue n >>> notNullA >>> arr Just)
+   `orElse` (constA Nothing)
+
+mbGetQAttrValue qn =
+   (getQAttrValue qn >>> notNullA >>> arr Just)
+   `orElse` (constA Nothing)
+
+
+-- ePub parsing helpers
+
+dcName, opfName, xmlName :: String -> QName
+dcName local = mkQName "dc" local "http://purl.org/dc/elements/1.1/"
+opfName local = mkQName "opf" local "http://www.idpf.org/2007/opf"
+xmlName local = mkQName "xml" local "http://FIXME-WRONG"
 
 
 opfPath :: (MonadError String m, MonadIO m) =>
@@ -41,61 +109,74 @@ opfPath zipPath = do
    containerContents <- extractFileFromZip zipPath
       "META-INF/container.xml"
 
-   let containerTags = parseTags containerContents
+   result <- liftIO $ runX (
+      readString hxtParseParams containerContents
+      >>> atTag "rootfile" >>> getAttrValue "full-path"
+      )
 
-   return $ fromAttrib "full-path" . head . head .
-         sections (~== "<rootfile>") $ containerTags
-
-
-extractTitle :: [Tag String] -> String
-extractTitle = fromTagText . head . filter isTagText
-   . head . (sections (~== "<dc:title>"))
-
-
-extractCreator :: [Tag String] -> Creator
-extractCreator tags = Creator role creator
-   where
-      creatorTags = head . (sections (~== "<dc:creator>")) $ tags
-
-      role = fromAttrib "opf:role" . head $ creatorTags
-
-      creator = fromTagText . head . filter isTagText
-         $ creatorTags
+   case result of
+      (p : []) -> return p
+      _        -> throwError
+         "ERROR: rootfile full-path missing from META-INF/container.xml"
 
 
-extractDates :: [Tag String] -> [Date]
-extractDates = map extractDate . sections (~== "<dc:date>")
-   where
-      extractDate ts = (event, date)
-         where
-            event = fromAttrib "opf:event" . head $ ts
-            date = fromTagText . head . filter isTagText $ ts
+getTitles = atQTag (dcName "title") >>>
+   proc x -> do
+      l <- mbGetQAttrValue (xmlName "lang") -< x
+      c <- text -< x
+      returnA -< Title l c
 
 
-extractEpubMeta :: (MonadIO m, MonadError String m) =>
-   FilePath -> m EpubMeta
-extractEpubMeta zipPath = EpubMeta
-   `liftM` (extractTitle `liftM` opfTags)
-   `ap`    (extractCreator `liftM` opfTags)
-   `ap`    (extractDates `liftM` opfTags)
+getCreators = atQTag (dcName "creator") >>>
+   proc x -> do
+      r <- mbGetQAttrValue (opfName "role") -< x
+      f <- mbGetQAttrValue (opfName "file-as") -< x
+      c <- text -< x
+      returnA -< Creator r f c
 
-   where
-      opfTags = do
-         opfContents <- extractFileFromZip zipPath =<< opfPath zipPath
 
-         return $ head . sections (~== "<metadata>") . parseTags
-            $ opfContents
+getDates = atQTag (dcName "date") >>>
+   proc x -> do
+      e <- mbGetQAttrValue (opfName "event") -< x
+      c <- text -< x
+      returnA -< Date e c
+
+
+getMeta = atTag "metadata" >>>
+   proc x -> do
+      ts <- getTitles -< x
+      cs <- getCreators -< x
+      ds <- getDates -< x
+      returnA -< emptyEpubMeta
+         { emTitles = [ts]
+         , emCreators = [cs]
+         , emDates = [ds]
+         }
+
+
+extractEpubMeta zipPath = do
+   opfContents <- extractFileFromZip zipPath =<< opfPath zipPath
+   let parsedDoc = readString hxtParseParams opfContents
+         >>> propagateNamespaces
+
+   --x <- liftIO $ runX ( parsedDoc >>> getTitles )
+   --x <- liftIO $ runX ( parsedDoc >>> getCreators )
+   --x <- liftIO $ runX ( parsedDoc >>> getDates )
+   x <- liftIO $ runX ( parsedDoc >>> getMeta )
+
+   return x
+   --return ()
 
 
 extractFileFromZip ::
    (MonadIO m, MonadError [Char] m) =>
    FilePath -> FilePath -> m String
 extractFileFromZip zipPath filePath = do
-   let app = "unzip"
+   let dearchiver = "unzip"
    result <- liftIO $ tryEC $ run
-      ((printf "%s -p %s %s" app zipPath filePath) :: String)
+      ((printf "%s -p %s %s" dearchiver zipPath filePath) :: String)
    case result of
       Left ps -> throwError $
          printf "[ERROR %s  zip file: %s  path in zip: %s  status: %s]"
-            app zipPath filePath (show ps)
+            dearchiver zipPath filePath (show ps)
       Right output -> return output
