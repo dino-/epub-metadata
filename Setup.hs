6c4a6e53b4e302bc663a4c28ef2fbda247737da9
Dino Morelli <dino@ui3.info>
2010-05-07 21:28:46 +0000

    Now using new Setup.hs with custom post-build and test targets

--- Setup.hs
+++ Setup.hs
@@ -1,10 +1,39 @@
 #! /usr/bin/env runhaskell
 
--- Copyright: 2010 Ren Hoek
+-- Copyright: 2010 Dino Morelli
 -- License: BSD3 (see LICENSE)
--- Author: Ren Hoek <ren@spumco.co.dk>
+-- Author: Dino Morelli <dino@ui3.info>
 
+import Control.Monad ( unless )
 import Distribution.Simple
+import System.Cmd ( system )
+import System.Directory ( createDirectoryIfMissing )
+import System.FilePath
+import System.Posix.Files ( createSymbolicLink, fileExist )
 
 
-main = defaultMain
+main = defaultMainWithHooks (simpleUserHooks 
+   { postBuild = customPostBuild
+   , runTests = testRunner
+   } )
+
+   where
+      -- Create symlink to the binary after build for developer 
+      -- convenience
+      customPostBuild _ _ _ _ = do
+         let binDir = "bin"
+         let binName = "epub-meta"
+         let destPath = binDir </> binName
+
+         createDirectoryIfMissing True binDir
+
+         linkExists <- fileExist destPath
+         unless linkExists $ do
+            createSymbolicLink
+               (".." </> "dist" </> "build" </> binName </> binName)
+               destPath
+
+      -- Target for running all unit tests
+      testRunner _ _ _ _ = do
+         system $ "runhaskell -isrc testsuite/runtests.hs"
+         return ()
