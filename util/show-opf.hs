b001927126895ad165fc28ecfa0d5decdf9544b0
Dino Morelli <dino@ui3.info>
2011-01-02 20:47:02 +0000

    Added a tool to dump raw Package data from a .opf file
    This was intended to assist with making and updating unit test data.

---
+++ util/show-opf.hs
@@ -0,0 +1,13 @@
+#! /usr/bin/runhaskell -isrc
+
+-- Copyright: 2010 Dino Morelli
+-- License: BSD3 (see LICENSE)
+-- Author: Dino Morelli <dino@ui3.info>
+
+import System.Environment ( getArgs )
+
+import Codec.Epub.Opf.Parse
+
+
+main :: IO ()
+main = (fmap head) getArgs >>= readFile >>= parseXmlToOpf >>= print
