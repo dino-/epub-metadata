24d0537424c1ae50bbeaecc711c13da7b832db52
Dino Morelli <dino@ui3.info>
2011-03-22 01:14:26 +0000

    Took control of building the list of files to be zipped
    This means we can leave out all directory entries (which is dictated
    by the strict epub rules) and leave out any dotfiles that may be in
    the directory.

--- src/Codec/Epub/Archive.hs
+++ src/Codec/Epub/Archive.hs
@@ -11,7 +11,26 @@ module Codec.Epub.Archive
 import Codec.Archive.Zip
 import Control.Monad
 import qualified Data.ByteString.Lazy as B
+import Data.List
 import System.Directory
+import System.FilePath
+
+
+{- Recursively get a list of all files starting with the supplied
+   parent directory. Excluding the directories themselves and ANY 
+   dotfiles.
+-}
+getRecursiveContents :: FilePath -> IO [FilePath]
+getRecursiveContents parent = do
+   fullContents <- getDirectoryContents parent
+   let contents = filter (not . isPrefixOf ".") fullContents
+   paths <- forM contents $ \name -> do
+      let path = parent </> name
+      isDirectory <- doesDirectoryExist path
+      if isDirectory
+         then getRecursiveContents path
+         else return [path]
+   return $ concat paths
 
 
 {- | Construct a zip Archive containing epub book data from the 
@@ -20,9 +39,12 @@ import System.Directory
 mkEpubArchive :: FilePath -> IO Archive
 mkEpubArchive rootDir = do
    setCurrentDirectory rootDir
+
+   allFiles <- getRecursiveContents "."
+
    foldM (flip id) emptyArchive
       [ flip (addFilesToArchive []) ["mimetype"]
-      , flip (addFilesToArchive [OptRecursive]) ["."]
+      , flip (addFilesToArchive [OptRecursive]) allFiles
       ]
 
 
