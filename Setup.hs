53061aee8dcbfb78836e37c7a198074c4021acec
Dino Morelli <dino@ui3.info>
2011-10-26 23:42:07 +0000

    Added a default Setup.hs to the project
    Turns out having a basic default Setup.hs is required.

---
+++ Setup.hs
@@ -0,0 +1,10 @@
+#! /usr/bin/env runhaskell
+
+-- Copyright: 2010, 2011 Dino Morelli
+-- License: BSD3 (see LICENSE)
+-- Author: Dino Morelli <dino@ui3.info>
+
+import Distribution.Simple
+
+
+main = defaultMain
