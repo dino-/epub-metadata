ff434411fa647c816f0fcb152cb4f6d0a10b3d76
Dino Morelli <dino@ui3.info>
2013-02-03 15:00:59 +0000

    Fixed show-opf utility to work with newer ErrorT parseXmlToOpf code

--- util/show-opf.hs
+++ util/show-opf.hs
@@ -4,10 +4,14 @@
 -- License: BSD3 (see LICENSE)
 -- Author: Dino Morelli <dino@ui3.info>
 
+import Control.Monad.Error ( runErrorT )
 import System.Environment ( getArgs )
 
 import Codec.Epub.Opf.Parse
 
 
 main :: IO ()
-main = (fmap head) getArgs >>= readFile >>= parseXmlToOpf >>= print
+main = do
+   opfXmlContents <- (fmap head) getArgs >>= readFile
+   parseResult <- runErrorT $ parseXmlToOpf opfXmlContents
+   either print print parseResult
