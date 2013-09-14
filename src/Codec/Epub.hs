bad46b6571f0c26d9a997b268a84c954525cac24
Dino Morelli <dino@ui3.info>
2013-09-14 13:26:21 +0000

    Added a convenience module to reexport commonly used modules in this library

---
+++ src/Codec/Epub.hs
@@ -0,0 +1,20 @@
+-- Copyright: 2013 Dino Morelli
+-- License: BSD3 (see LICENSE)
+-- Author: Dino Morelli <dino@ui3.info>
+
+{- | Overview docs and usage examples go here
+-}
+module Codec.Epub
+   ( module Codec.Epub.IO
+   , module Codec.Epub.Data.Metadata
+   , module Codec.Epub.Data.Package
+   , module Codec.Epub.Format
+   , module Codec.Epub.Parse
+   )
+   where
+
+import Codec.Epub.IO
+import Codec.Epub.Data.Metadata
+import Codec.Epub.Data.Package
+import Codec.Epub.Format
+import Codec.Epub.Parse
